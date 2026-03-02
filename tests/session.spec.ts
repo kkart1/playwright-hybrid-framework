7ce8e74ff0cbf9f82a2142f2280adb7ae0947e94
kkart1 <karthick.kesav1991@gmail.com>
2026-03-02 19:41:08 +0530

    Fix 4 Removed Session Spec file

--- tests/session.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '../fixtures/baseTest';
-
-test('Verify session reused without login', async ({ page }) => {
-
-  await page.goto('https://the-internet.herokuapp.com/secure');
-
-  await expect(page.locator('h2')).toHaveText('Secure Area');
-
-});
