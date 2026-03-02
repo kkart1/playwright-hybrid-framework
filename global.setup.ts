18bceeec3987f8807fd0e224fedb0285bb3a60ff
kkart1 <karthick.kesav1991@gmail.com>
2026-03-02 14:05:08 +0530

    Fix global setup for CI headless mode

--- global.setup.ts
+++ global.setup.ts
@@ -1,13 +1,12 @@
-// global.setup.ts
-
 import { chromium, FullConfig } from '@playwright/test';
 import config from './utils/configReader';
 
 async function globalSetup(_: FullConfig) {
 
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch({
+    headless: process.env.CI ? true : false
+  });
 
-  // IMPORTANT: create explicit context
   const context = await browser.newContext();
 
   const page = await context.newPage();
@@ -20,15 +19,13 @@ async function globalSetup(_: FullConfig) {
 
   await page.click('button[type="submit"]');
 
-  await page.waitForURL('**/secure');
+  await page.waitForTimeout(2000);
 
-  // Save authenticated state
   await context.storageState({
     path: 'auth/storageState.json',
   });
 
   await browser.close();
-
 }
 
 export default globalSetup;
